9eb02fdef448dba28f53324be5eed5661f3297d4
chenchangyuan <chenchangyuan@baidu.com>
2017-01-26 10:52:37 +0800

    delete

--- Moocer/minghua/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import QuizInfo
-from .models import SchoolInfo
-
-admin.site.register(QuizInfo)
-admin.site.register(SchoolInfo)
--- Moocer/polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import MinghuaQuizInfo
-from .models import MinghuaSchoolInfo
-
-admin.site.register(MinghuaQuizInfo)
-admin.site.register(MinghuaSchoolInfo)
