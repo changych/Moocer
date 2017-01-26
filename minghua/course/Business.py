6c4e75a0364e11b73712ffa97add1462a25b710b
chenchangyuan <chenchangyuan@baidu.com>
2017-01-26 15:57:19 +0800

    school

--- minghua/course/Business.py
+++ minghua/course/Business.py
@@ -12,7 +12,7 @@ class Business(object):
 		schoolInfoList = schoolHandler.getSchool(school)
 		for schoolInfo in schoolInfoList:
 			print(schoolInfo.key)
-			login = Login(schoolKey)
+			login = Login(schoolInfo.key)
 			res,browser = login.login(userName, password)
 			break
 		#schoolKey = schoolInfo['key']
