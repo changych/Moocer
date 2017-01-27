653bbacd7b6fc84d2fab33f7c795fe3192f7f89a
chenchangyuan <chenchangyuan@baidu.com>
2017-01-28 00:23:56 +0800

    school list

--- minghua/models.py
+++ minghua/models.py
@@ -34,7 +34,7 @@ class OrderInfo(models.Model):
     study = models.CharField(max_length=256)
     exam = models.CharField(max_length=256)
     status = models.IntegerField(default=0)
-    create_time = models.DateTimeField(auto_now_add=True)
+    create_time = models.DateTimeField(auto_now_add=True,default=datetime.now)
     finish_time = models.DateTimeField(default=0)
 
     def __str__(self):
