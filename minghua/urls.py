06e00f70d5a353024652f150a2fddcf9038de1df
chenchangyuan <chenchangyuan@baidu.com>
2017-01-27 11:41:06 +0800

    school list

--- minghua/urls.py
+++ minghua/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^addSchool/$', views.addSchool, name='addSchool'),
     url(r'^updateSchool/$', views.updateSchool, name='updateSchool'),
     url(r'^getSchoolList/$', views.getSchoolList, name='getSchoolList'),
     url(r'^getCourse/$', views.getCourse, name='getCourse'),
