1b62c4d9d669b355a28d2f6a94894184666ecc0f
chenchangyuan <chenchangyuan@baidu.com>
2017-01-30 14:07:22 +0800

    quiz

---
+++ minghua/quiz/quiz.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-  
+import requests
+import json
+import pyquery
+import time
+import traceback
+import sys
+
+from django.http import HttpResponse
+from minghua.models import QuizInfo
+
+class Quiz(object):
+
+	def getQuizInfo(self, quizContent):
+		q = QuizInfo.objects.filter(quiz_content=quizContent)
+		if(len(q) == 0):
+			return None
+		else:
+			return q
