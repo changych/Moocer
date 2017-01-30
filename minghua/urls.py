b1bc41934dcfb530f2568f3175929b61f0c3a135
chenchangyuan <chenchangyuan@baidu.com>
2017-01-30 14:40:46 +0800

    quiz

--- minghua/urls.py
+++ minghua/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^getSchoolList/$', views.getSchoolList, name='getSchoolList'),
     url(r'^getCourse/$', views.getCourse, name='getCourse'),
     url(r'^getQuiz/$', views.getQuiz, name='getQuiz'),
+    url(r'^addQuiz/$', views.addQuiz, name='addQuiz'),
     url(r'^addOrder/$', views.addOrder, name='addOrder'),
 ]
