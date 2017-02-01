7e96a76f6bd05bc793dfce4768b588452a14c11c
chenchangyuan <chenchangyuan@baidu.com>
2017-02-01 23:37:02 +0800

    wechat

--- minghua/wechat/Validate.py
+++ minghua/wechat/Validate.py
@@ -6,9 +6,10 @@ class Validate(object):
 	def validate(self, signature, timestmp, nonce):
 		token = '715074363yuan'
 		params = [token, timestmp, nonce]
+		params.sort()
 		rawStr = ''.join(params)
 		print(rawStr)
-		shaStr = hashlib.sha1(rawStr.encode('utf-8'))
+		shaStr = hashlib.sha1(rawStr.encode('utf-8')).hexdigest()
 		print(shaStr)
 		if shaStr == signature:
 			return True
