f06748018cff7350db2b7e3440856aa93848c4a3
chenchangyuan <chenchangyuan@baidu.com>
2017-02-02 18:08:29 +0800

    wechat

--- minghua/wechat/Validate.py
+++ minghua/wechat/Validate.py
@@ -32,7 +32,7 @@ class Validate(object):
 
 	def msg(self, data):
 		textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
-
+		print(data)
 		xml = etree.fromstring(data)
 		fromUserName = xml.find('FromUserName').text
 		toUserName = xml.find('ToUserName').text
