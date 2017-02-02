27187708d142074e4243b8eb27fd49aea5e7e452
chenchangyuan <chenchangyuan@baidu.com>
2017-02-02 21:20:02 +0800

    wechat

--- minghua/user/user.py
+++ minghua/user/user.py
@@ -7,26 +7,30 @@ from minghua.models import UserInfo
 
 class User(object):
 
-	def update(self, openId, schoolKey, schoolId, schoolPwd):
+	def update(self, openId, schoolKey, schoolId, schoolPwd, name):
 		u = UserInfo.objects.filter(open_id=openId)
 		if len(u) == 0:
 			schoolKey = '' if schoolKey == None else schoolKey
 			schoolId = '' if schoolId == None else schoolId
 			schoolPwd = '' if schoolPwd == None else schoolPwd
+			name = '' if name == None else name
 			u = UserInfo(
 				open_id=openId, 
 				school_key=schoolKey, 
 				school_id=schoolId, 
 				school_password=schoolPwd, 
+				name=name, 
 				create_time=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 			)
 			u.save()
 		else:
-			schoolKey = u.school_key if schoolKey == None else schoolKey
-			schoolId = u.school_id if schoolId == None else schoolId
-			schoolPwd = u.school_password if schoolPwd == None else schoolPwd
+			schoolKey = u[0].school_key if schoolKey == None else schoolKey
+			schoolId = u[0].school_id if schoolId == None else schoolId
+			schoolPwd = u[0].school_password if schoolPwd == None else schoolPwd
+			name = u[0].name if name == None else name
 			u.update(
 				school_key=schoolKey, 
 				school_id=schoolId, 
 				school_password=schoolPwd, 
+				name=name, 
 			)
