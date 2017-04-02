3fc716e55cdfa5225252727180d983bedfa4e1dd
chenchangyuan <chenchangyuan@baidu.com>
2017-04-02 22:49:36 +0800

    school

--- minghua/school/school.py
+++ minghua/school/school.py
@@ -43,7 +43,7 @@ class School(object):
 				)
 
 	def getSchool(self, school):
-		s = SchoolInfo.objects.get(ch_name__contains=school) 
+		s = SchoolInfo.objects.filter(ch_name__contains=school) 
 		print(s)
 		if(len(s) == 0):
 			return None
