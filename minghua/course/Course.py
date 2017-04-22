3dbea3bace07abd2b1af63ba67bc9b3cda843ae1
chenchangyuan <chenchangyuan@baidu.com>
2017-04-22 20:48:22 +0800

    course

--- minghua/course/Course.py
+++ minghua/course/Course.py
@@ -34,14 +34,24 @@ class Course(object):
 		j = pyquery.PyQuery(courseRes.text)
 		#print courseRes.content
 		#print j('.view-shadow')
-		for li in j('.view-item'):
-			title = pyquery.PyQuery(li)('.view-title').text()
-			subTitile = pyquery.PyQuery(li)('.view-subtitle')
-			link = pyquery.PyQuery(subTitile)('a').filter('.link-action')
-			href = pyquery.PyQuery(link).attr('href')
-			href = href[ : href.rfind('/')]
-			courseId = href[href.rfind('/')+1 : ]
-			print(courseId)
+
+		for li in j('li'):
+			href = pyquery.PyQuery(li)('.view-shadow').attr('href')
+			if href == None:
+				continue
+			courseId = href[href.rfind('/')+1 : len(href)-len('.mooc')]
+
+
+
+
+		# for li in j('.view-item'):
+		# 	title = pyquery.PyQuery(li)('.view-title').text()
+		# 	subTitile = pyquery.PyQuery(li)('.view-subtitle')
+		# 	link = pyquery.PyQuery(subTitile)('a').filter('.link-action')
+		# 	href = pyquery.PyQuery(link).attr('href')
+		# 	href = href[ : href.rfind('/')]
+		# 	courseId = href[href.rfind('/')+1 : ]
+		# 	print(courseId)
 
 			video_remain = 0
 			video_complete = 0
@@ -51,6 +61,7 @@ class Course(object):
 			videoListRes = self.browser.session.get(self.baseUri + '/portal/session/unitNavigation/' + str(courseId) + '.mooc')
 			jj = pyquery.PyQuery(videoListRes.content)
 			videoListDiv = jj('#unitNavigation')
+			title = jj('.model-title').attr('title')
 
 			for div in videoListDiv('i').items():
 				status = pyquery.PyQuery(div).attr('class')
@@ -63,13 +74,37 @@ class Course(object):
 				elif status.find('icon-edit02') != -1:
 					test_remain = test_remain + 1
 
+
+			examStart = ''
+			examEnd = ''
+			examScore = 0
+			examFlag = 1 if videoListRes.content.find('线上考试') != -1 else 0
+			if examFlag == 1:
+				examListRes = self.browser.session.get(self.baseUri + '/portal/examine/list/' + str(courseId) + '.mooc')
+				itemList = pyquery.PyQuery(examListRes.content)('.flipped-item')
+				for item in itemList:
+					examId = pyquery.PyQuery(item)('.btn-area').attr('examineid')
+					timeList = pyquery.PyQuery(item)('.fclass-para')
+					for time in timeList:
+						text = pyquery.PyQuery(time).text()
+						if text.find('开始') != -1:
+							examStart = text[text.find('：')+1:]
+						if text.find('截止') != -1:
+							examEnd = text[text.find('：')+1:]
+						if text.find('得分') != -1:
+							examScore = text[text.find('：')+1:]
+							examScore = examScore[:len(examScore)-1]
+
 			courseSet.append({
 				'id': courseId, 
 				'title':title, 
-				'video-remain': video_remain,
+				'video_remain': video_remain,
 				'video_complete': video_complete,
 				'test_remain': test_remain,
-				'test_complete': test_complete
+				'test_complete': test_complete,
+				'exam_start': examStart,
+				'exam_end': examEnd,
+				'exam_score': examScore
 			})
 
 		print(courseSet)
