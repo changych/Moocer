c781633c0d5d13c82e99e15c2d55352d6be573a7
chenchangyuan <chenchangyuan@baidu.com>
2017-04-22 17:34:23 +0800

    course

--- minghua/views.py
+++ minghua/views.py
@@ -65,9 +65,9 @@ def getCourse(request):
 	business = Business()
 	try:
 		courseSet = business.run(
-			request.REQUEST['school_id'], 
-			request.REQUEST['user_id'], 
-			request.REQUEST['password']
+			request.GET['school_id'], 
+			request.GET['user_id'], 
+			request.GET['password']
 		)
 		return HttpResponse(json.dumps(courseSet))
 	except: 
