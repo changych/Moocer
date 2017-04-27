b5827d34594647517fefc69a69116e79139c8a6b
chenchangyuan <chenchangyuan@baidu.com>
2017-04-27 11:16:23 +0800

    exam

--- minghua/course/Business.py
+++ minghua/course/Business.py
@@ -5,14 +5,32 @@ import os
 from minghua.school.school import School
 from minghua.course.Login import Login
 from minghua.course.Course import Course
+from minghua.record.record import Record
 
 class Business(object):
 
 	def run(self, school, userName, password):
-		login = Login(school)
-		res,browser = login.login(userName, password)
-		course = Course(school, browser, userName)
-		courseSet = course.courseList()
-		#schoolKey = schoolInfo['key']
-		#print(schoolKey)
-		return courseSet
+		r = RecordInfo.objects.filter(school=school).filter(user=userName)
+		if(len(r) > 0):
+			courseSet = []
+			for record in r:
+				courseSet.append({
+					'id': record.courseid, 
+					'title':course.course_title, 
+					'video_remain': record.videoremain,
+					'video_complete': record.videocomplete,
+					'test_remain': record.testremain,
+					'test_complete': record.testcomplete,
+					'exam_start': record.exam_start,
+					'exam_end': record.exam_end,
+					'exam_score': record.score
+				})
+			return courseSet
+		else:
+			login = Login(school)
+			res,browser = login.login(userName, password)
+			course = Course(school, browser, userName)
+			courseSet = course.courseList()
+			#schoolKey = schoolInfo['key']
+			#print(schoolKey)
+			return courseSet
