36c4b9d8da01c3b100316d6d5b20115e755142e3
chenchangyuan <chenchangyuan@baidu.com>
2017-05-10 14:54:25 +0800

    course

--- minghua/course/Business.py
+++ minghua/course/Business.py
@@ -31,48 +31,48 @@ class Business(object):
 
 	def run(self, school, userName, password):
 		now = datetime.datetime.now()
-		r = RecordInfo.objects.filter(school=school).filter(user=userName)
-		if(len(r) > 0):
-			courseSet = []
-			for record in r:
-				courseSet.append({
-					'id': record.courseid, 
-					'title':record.course_title, 
-					'video_remain': record.videoremain,
-					'video_complete': record.videocomplete,
-					'test_remain': record.testremain,
-					'test_complete': record.testcomplete,
-					'exam_start': str(record.exam_start),
-					'exam_end': str(record.exam_end),
-					'exam_score': record.score,
-					'study_status': record.study_status,
-					'exam_status': record.exam_status
-				})
-			return courseSet
-		else:
-			login = Login(school)
-			res,browser = login.login(userName, password)
-			course = Course(school, browser, userName)
-			courseSet = course.courseList()
-			record = Record()
-			for course in courseSet:
-				res = record.addRecord(
-					school,
-					userName,
-					password,
-					course['id'],
-					course['title'],
-					course['video_remain'],
-					course['video_complete'],
-					course['test_remain'],
-					course['test_complete'],
-					course['exam_score'],
-					course['exam_start'],
-					course['exam_end'],
-					0,
-					0,
-					now.strftime('%Y-%m-%d %H:%M:%S')
-				)
-			#schoolKey = schoolInfo['key']
-			#print(schoolKey)
-			return courseSet
+		# r = RecordInfo.objects.filter(school=school).filter(user=userName)
+		# if(len(r) > 0):
+		# 	courseSet = []
+		# 	for record in r:
+		# 		courseSet.append({
+		# 			'id': record.courseid, 
+		# 			'title':record.course_title, 
+		# 			'video_remain': record.videoremain,
+		# 			'video_complete': record.videocomplete,
+		# 			'test_remain': record.testremain,
+		# 			'test_complete': record.testcomplete,
+		# 			'exam_start': str(record.exam_start),
+		# 			'exam_end': str(record.exam_end),
+		# 			'exam_score': record.score,
+		# 			'study_status': record.study_status,
+		# 			'exam_status': record.exam_status
+		# 		})
+		# 	return courseSet
+		# else:
+		login = Login(school)
+		res,browser = login.login(userName, password)
+		course = Course(school, browser, userName)
+		courseSet = course.courseList()
+		record = Record()
+		for course in courseSet:
+			res = record.addRecord(
+				school,
+				userName,
+				password,
+				course['id'],
+				course['title'],
+				course['video_remain'],
+				course['video_complete'],
+				course['test_remain'],
+				course['test_complete'],
+				course['exam_score'],
+				course['exam_start'],
+				course['exam_end'],
+				0,
+				0,
+				now.strftime('%Y-%m-%d %H:%M:%S')
+			)
+		#schoolKey = schoolInfo['key']
+		#print(schoolKey)
+		return courseSet
